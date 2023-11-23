e09047e174bc13c8eed24394f9902750abf643cf
dronoff2032 <isip_a.a.zhirnov@mpt.ru>
2023-11-23 15:07:23 +0300

    проводник

---
+++ Explorer/Explorer/Menu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer
+{
+    internal class Menu
+    {
+        public static int show(int min_y, int max_y)
+        {
+            int y = min_y;
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, y);
+                Console.WriteLine(">>");
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                Console.SetCursorPosition(0, y);
+                Console.WriteLine("  ");
+
+                if (key.Key == ConsoleKey.UpArrow && y != min_y)
+                {
+                    y--;
+                }
+                else if (key.Key == ConsoleKey.DownArrow && y != max_y + min_y)
+                {
+                    y++;
+                }
+                else if (key.Key == ConsoleKey.Escape)
+                {
+                    return -1;
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    return y - min_y;
+                }
+            }
+
+        }
+    }
+}
---
+++ Explorer/Explorer/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+
+namespace Explorer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = null;
+
+            DriveInfo[] all_drives = DriveInfo.GetDrives();
+
+            while (true)
+            {
+                if (path == null)
+                {
+                    foreach (DriveInfo drive in all_drives)
+                    {
+                        Console.WriteLine("   " + drive.Name + " | Название диска: " + drive.VolumeLabel + " | Свободно " + drive.TotalFreeSpace / 1024 / 1024 / 1024 +  " GB из " + drive.TotalSize / 1024 / 1024 / 1024 + " GB");
+                    }
+                    int choice = Menu.show(0, all_drives.Length - 1);
+
+                    if (choice != -1)
+                    {
+                        path += all_drives[choice].Name;
+                    }
+                } else
+                {
+                    string[] directories = Directory.GetDirectories(path);
+                    string[] files = Directory.GetFiles(path);
+
+                    Console.WriteLine("Путь: " + path);
+                    Console.WriteLine(" -- -- -- -- -- -- -- -- -- -- -- -- -- --");
+                    foreach (string driectory in directories)
+                    {
+                        Console.WriteLine("   " + driectory.Replace(path, "").Replace("\\", ""));
+                    }
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine("   " + file.Replace(path, "").Replace("\\", ""));
+                    }
+
+                    if (directories.Length != 0 || files.Length != 0)
+                    {
+                        int choice = Menu.show(2, directories.Length + files.Length - 1);
+
+                        if (choice == -1)
+                        {
+                            path = Path.GetDirectoryName(path);
+                        }
+                        else if (choice <= directories.Length - 1)
+                        {
+                            // Folders
+                            path += directories[choice].Replace(path, "");
+                        }
+                        else
+                        {
+                            Process.Start(new ProcessStartInfo { FileName = path + files[choice - directories.Length].Replace(path, ""), UseShellExecute = true });
+                        }
+                    } else
+                    {
+                        int choice = Menu.show(2, 0);
+
+                        if (choice == -1)
+                        {
+                            path = Path.GetDirectoryName(path);
+                        }
+                    }
+                }
+
+                Console.Clear();
+            }
+        }
+    }
+}
